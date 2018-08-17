2deead7b90993f4d6d39f9c02da5744a9daea3ae
tzlibre <tzlibre@protonmail.com>
2018-08-17 09:05:49 +0000

    commit

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CONTRACT_ALIAS="bond-bank-v1.0"
+OWNER_ALIAS="bond-bank-owner"
+BAKER_ALIAS="baker01"
+ONEP6=1000000
+ONEP3=1000
+
+##################################################
+# deploy contract
+##################################################
+OWNER="tz1T4H8uQerqskAVxCx6CHXsADvKqr88kyh2"
+MAX_DEPOSITORS=100
+MIN_FUNDING=$[1000*$ONEP6] # 1000 XTZ
+WITHDRAW_FEE=$ONEP3        # 0.1% 
+COLL_COEFF=$[500*$ONEP3]   # 50%
+
+tezos-client originate contract $CONTRACT_ALIAS for $OWNER_ALIAS \
+transferring 0 from $OWNER_ALIAS running bond-bank.liq.tz \
+--init "Pair \"$OWNER\" (Pair $MAX_DEPOSITORS (Pair $MIN_FUNDING (Pair $WITHDRAW_FEE (Pair $COLL_COEFF (Pair 0 (Pair 0 (Pair {} 0)))))))" \
+--delegate $BAKER_ALIAS --delegatable 
+##################################################
